6ce4db647af651ecf9bd307c2927796d24b18fde
Daniel Buompane <danielbuompane@gmail.com>
2016-01-24 14:15:51 -0500

    Fixed bugs

--- prework.rb
+++ prework.rb
@@ -3,18 +3,19 @@ name = gets.chomp.capitalize
 puts "Hi #{name}! Want to see how smart I am?"
 answer = gets.chomp.downcase
 
-if answer.include? "no"
-  puts "Your loss. Have a nice day #{name}!"
-else
+if answer.include? "yes"
   puts "Great! What year were you born?"
-end 
-year = gets.chomp.to_i
-age = 2015 - year
-
-puts "#{name}, you are #{age} year's old. This lists the years you were alive. I bet it would be fun to go back and find the biggest historical event from each year. "
+  year = gets.chomp.to_i
+  age = 2015 - year
+puts "#{name}, you are #{age} year's old. This lists the years you were alive.
+You should try to find the biggest historical event from each year.
+Thanks for letting me show off my Ruby skills."
 
 for num in year...2016
 	puts num
+	break if num <= 1900
 end
+else
+  puts "Your loss. Have a nice day #{name}!"
+ end
 
-puts "#{name}, thanks for letting me show off my fancy Ruby skills!"
